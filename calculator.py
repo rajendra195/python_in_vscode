1512942524bdf846caf40bb217e1a56eb6d460de
rajendra195 <rajendraniroula12@gmail.com>
2024-02-05 11:44:17 +0545

    added sum  operations

--- calculator.py
+++ calculator.py
@@ -1,7 +1,15 @@
 class Opr:
+    def __init__(self):
+        pass
+
+    def add(self, *args):
+        return sum(list(args))
+    
     def diff(self, n, m):
         return n - m
     
 o = Opr()
 q = o.diff(n=4, m=3)
 print(q)
+r = o.add(3, 32,33, 2, 47, 29, 10)
+print(r)
